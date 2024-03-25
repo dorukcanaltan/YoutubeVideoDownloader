96c2d266c18d1d0cf9fb30b3edfd1bf40214f40e
Doruk Can Altan <dorukcanaltan@hotmail.com>
2024-03-25 20:35:03 +0300

    comments deleted

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pytube import YouTube
 
-# İlerleme durumunu gösteren fonksiyon
+
 def on_progress(stream, chunk, bytes_remaining):
     total_size = stream.filesize
     bytes_downloaded = total_size - bytes_remaining
     percentage_of_completion = bytes_downloaded / total_size * 100
     print(f'İndirme Durumu: %{percentage_of_completion:.2f} tamamlandı.')
 
-# İndirme tamamlandığında çağrılacak fonksiyon
+
 def on_complete(stream, file_path):
     print(f'İndirme tamamlandı. Dosya yolu: {file_path}')
 
@@ -16,7 +16,7 @@ def download_video(url, path=''):
     yt.register_on_progress_callback(on_progress)
     yt.register_on_complete_callback(on_complete)
 
-    # Yüksek çözünürlüklü stream'i seçip indiriyoruz.
+
     yt.streams.get_highest_resolution().download(output_path=path)
 
 url = input("Enter the URL: ")
